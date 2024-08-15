2590db307ae3a3e660e3e84cd8f8c6359c5d95b8
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-15 20:25:13 +0900

    java/com.example/blog_appにcontrollerパッケージとPostController.javaを作成する

--- src/main/java/com/example/blog_app/controller/PostController.java
+++ src/main/java/com/example/blog_app/controller/PostController.java
@@ -1,8 +1,14 @@
 package com.example.blog_app.controller;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 @Controller
 @RestController
 public class PostController {
