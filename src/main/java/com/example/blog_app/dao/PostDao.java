9c098bc081da71c84d82503f0667edca869a2715
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-16 10:55:29 +0900

    java/com.example/blog_appにdaoパッケージとPostDao.javaを作成する

--- src/main/java/com/example/blog_app/dao/PostDao.java
+++ src/main/java/com/example/blog_app/dao/PostDao.java
@@ -3,6 +3,6 @@ package com.example.blog_app.dao;
 import com.example.blog_app.bean.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostDao implements JpaReponsitory<Post, Long> {
+public interface PostDao extends JpaReponsitory<Post, Long> {
 
 }
