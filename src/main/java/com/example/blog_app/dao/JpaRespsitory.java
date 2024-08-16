815c428d088e9de33f7699b9245e3d15cd0fc451
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-16 10:16:53 +0900

    java/com.developer/employee_managementにcontrollerパッケージとEmployeeController.javaを作成する

--- src/main/java/com/example/blog_app/dao/JpaRespsitory.java
+++ src/main/java/com/example/blog_app/dao/JpaRespsitory.java
@@ -1,5 +1,6 @@
 package com.example.blog_app.dao;
 
+import org.hibernate.mapping.List;
 import org.springframework.data.repository.*;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 
@@ -9,10 +10,10 @@ public interface JpaRespsitory<T, ID>
 
     <S extends T> S saveAllFlush(S entity);
 
-    <S extends T> List<S> saveAllFlush(Interable<S> entities);
+    <S extends T> List<S> saveAllFlush(Iterable<S> entities);
 
     @Deprecated
-    default void deleteInBatch(Interable<T> entities) {
+    default void deleteInBatch(Iterable<T> entities) {
         this.deleteAllInBatch(entities);
     }
 }
