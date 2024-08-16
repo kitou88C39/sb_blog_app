54028e11880db5e4b257f6cb76e4a4b6899a941d
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-16 20:46:32 +0900

    java/com.example/blog_appにcontrollerパッケージとPostController.javaを作成する

--- src/main/java/com/example/blog_app/controller/PostController.java
+++ src/main/java/com/example/blog_app/controller/PostController.java
@@ -3,11 +3,19 @@ package com.example.blog_app.controller;
 import com.example.blog_app.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @PostMapping("path")
+    public String postMethodName(@RequestBody String entity) {
+
+        return entity;
+    }
 
 }
