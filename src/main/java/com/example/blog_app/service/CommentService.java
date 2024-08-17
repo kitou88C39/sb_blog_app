1715624d7098907969ee521c7ba677edb8d029aa
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-17 21:00:31 +0900

    java/com.example/blog_app/serviceにCommentService.javaを作成する

--- src/main/java/com/example/blog_app/service/CommentService.java
+++ src/main/java/com/example/blog_app/service/CommentService.java
@@ -1,11 +1,17 @@
 package com.example.blog_app.service;
 
+import com.example.blog_app.dao.PostDao;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
 
+    private PostDao postDao;
+
     // addComment()
     // getCommentByCommentId()
+    // getCommentByPostId()
+    // updateComment()
+    // deleteComment()
 
 }
