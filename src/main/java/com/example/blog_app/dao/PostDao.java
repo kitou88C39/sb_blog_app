883f78fcf650481ad1a0b4119f673ef59df350b5
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-18 14:35:11 +0900

    java/com.example/blog_app/serviceにPosttService.javaを作成する

--- src/main/java/com/example/blog_app/dao/PostDao.java
+++ src/main/java/com/example/blog_app/dao/PostDao.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PostDao extends JpaReponsitory<Post, Long> {
+public interface PostDao extends JpaRepository<Post, Long> {
 
 }
