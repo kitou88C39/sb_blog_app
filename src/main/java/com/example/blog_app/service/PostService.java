faad658571e5eedef784fb89b5e212f992a6e004
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-18 11:24:22 +0900

    java/com.example/blog_app/serviceにPosttService.javaを作成する

--- src/main/java/com/example/blog_app/service/PostService.java
+++ src/main/java/com/example/blog_app/service/PostService.java
@@ -7,6 +7,8 @@ import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class PostService {
 
