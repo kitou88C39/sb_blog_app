5bd27a7245e3f82512c5dc4eaf6075cd15ebe115
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-18 14:43:50 +0900

    java/com.example/blog_appにcontrollerパッケージとCommentController.javaを作成する

---
+++ src/main/java/com/example/blog_app/controller/CommentController.java
@@ -0,0 +1,5 @@
+package com.example.blog_app.controller;
+
+public class CommentController {
+
+}
--- src/main/java/com/example/blog_app/controller/PostController.java
+++ src/main/java/com/example/blog_app/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@CommentController
 public class PostController {
 
     @Autowired
