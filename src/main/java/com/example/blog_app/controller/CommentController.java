d428c63ac01aad9d59e85c0ec9923314dfeb6264
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-18 16:19:22 +0900

    java/com.example/blog_app/controller/CommentController.javaを作成する

--- src/main/java/com/example/blog_app/controller/CommentController.java
+++ src/main/java/com/example/blog_app/controller/CommentController.java
@@ -2,6 +2,8 @@ package com.example.blog_app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.blog_app.service.CommentService;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,8 +14,10 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("/posts/{id}/createComment")
-    public ResponseEntity<String> createComment(@PathVariable Long id, @RequestBody Comment comment) {
-        commentService.addComment(comment, id)
+    public ResponseEntity<String> createComment(@RequestBody Comment comment, @PathVariable Long id) {
+        commentService.addComment(comment, id);
+
+        return new ResponseEntity<>("Post created successfully. Id -> " + response.getId(), HttpStatus.CREATED);
     }
 
 }
