5eb9ac3e5f458ebd971faf2997541870418470de
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-19 16:08:17 +0900

    java/com.example/blog_app/controllerにCommentController.javaを作成する

--- src/main/java/com/example/blog_app/controller/CommentController.java
+++ src/main/java/com/example/blog_app/controller/CommentController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class CommentController {
@@ -32,6 +35,13 @@ public class CommentController {
     @GetMapping("/post/{id}/getComments")
     public List<Comment> getCommentByPostId(@PathVariable Long id) {
         return commentService.getCommentByPostId(id);
+    }
+
+    @PutMapping("path/{id}")
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        // TODO: process PUT request
+
+        return entity;
     }
 
 }
